ed23c82e7fd49e37f81bfc39243e659b0220a41b
Jacopo <jcp.federico@gmail.com>
2023-02-28 15:02:36 +0100

    update eser 5 funzionante

--- eser5ArrayOrdinato.cpp
+++ eser5ArrayOrdinato.cpp
@@ -5,3 +5,35 @@ altrimenti. */
 #include <iostream>
 #include <array>
 using namespace std;
+
+bool arrayOrdine(int arrayNumbers[], int arrayLength)
+{
+    bool ordine = true; 
+    for(int i = 0; i < arrayLength; i++) // Mi scorro l'array
+    {
+        if(arrayNumbers[i] > arrayNumbers[i +1]) // Comparo l'array[i] con il successivo(array[i +1])
+        {
+            return true; 
+        }
+        else
+        {
+            return false;
+            break;
+        }
+    }
+    return ordine;
+}
+
+int main ()
+{
+    int arrayNumbers[5] = {1, 2, 3, 4, 5};
+    int arrayLength = sizeof(arrayNumbers) / sizeof(arrayNumbers[0]);
+    if(arrayOrdine(arrayNumbers, arrayLength) == false)
+    {
+        cout << "L'array è in ordine crescente. \n";
+    }
+    else
+    {
+        cout << "L'array è in ordine decrescente. \n";
+    }
+}
